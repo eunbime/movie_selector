20abe46feb44cd22d4729c5820db9e9f42fdd623
eunbime <ebk1014@gmail.com>
2023-10-18 17:20:50 +0900

    docs

---
+++ js/mainMovie.js
@@ -0,0 +1,9 @@
+movies = [
+  // 여러 개의 movie 객체를 배열로 저장한다.
+];
+
+const title = document.querySelector(".movie_info .movie_title");
+const rating = document.querySelector(".movie_info .movie_rating");
+const summary = document.querySelector(".movie_info .movie_summary");
+
+console.log(title, rating, summary);
