b2fabdcca6c617f5eb1169dd575de37a0ff2f412
eunbime <ebk1014@gmail.com>
2023-10-19 15:41:15 +0900

    docs

--- js/mainMovie.js
+++ js/mainMovie.js
@@ -1,9 +1,5 @@
 const API_KEY = "7ea5a4480f6e34a1f8f87e7241924dd2";
 
-// movies = [
-//   // 여러 개의 movie 객체를 배열로 저장한다.
-// ];
-
 const options = {
   method: "GET",
   headers: {
@@ -19,22 +15,74 @@ fetch(
 )
   .then((response) => response.json())
   .then((data) => {
+    const movies = data.results;
+
+    // 헤더 영화 랜덤 보여주기
     const title = document.querySelector(".movie_info .movie_title");
     const rating = document.querySelector(".movie_info .movie_rating");
     const summary = document.querySelector(".movie_info .movie_summary");
     const image = document.querySelector(".movie_header_image");
-    const movieImg = document.createElement("img");
     const header = document.querySelector(".header");
-    const movies = data.results;
     const rndMovie = movies[Math.floor(Math.random() * movies.length)];
+
     const backdropPath = `https://image.tmdb.org/t/p/w500/${rndMovie.backdrop_path}`;
     const posterPath = `https://image.tmdb.org/t/p/w500/${rndMovie.poster_path}`;
+    const movieImg = document.createElement("img");
     movieImg.src = posterPath;
+    image.appendChild(movieImg);
     header.style.backgroundImage = `url(${backdropPath})`;
 
-    image.appendChild(movieImg);
     title.innerText = rndMovie.title;
     rating.innerText = rndMovie.vote_average;
     summary.innerText = rndMovie.overview;
+
+    // 영화 검색하기 => filter 사용
+    const search = document.querySelector("#search");
+    const searchIcon = document.querySelector("#searchIcon");
+
+    function paintResult(e) {
+      e.preventDefault();
+      const resultList = movies.filter((movie) => movie.title === search.value);
+      search.value = "";
+      console.log(resultList);
+    }
+
+    searchIcon.addEventListener("click", paintResult);
+
+    // 영화 카드 불러오기
+    let listCards = document.querySelector(".movie_cards");
+    movies.map((movie) => {
+      const listCard = document.createElement("div");
+      const listImg = document.createElement("img");
+      const listTitle = document.createElement("h3");
+      const listRating = document.createElement("span");
+      const posterPath = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+      listImg.src = posterPath;
+      listCard.appendChild(listImg);
+      listTitle.innerText = movie.title;
+      listRating.innerText = movie.vote_average;
+      listCard.appendChild(listTitle);
+      listCard.appendChild(listRating);
+      listCards.appendChild(listCard);
+
+      function cardRotate(e) {
+        e.preventDefault();
+        // alert(`id=${movie.id}`);
+        const target = event.currentTarget;
+        target.style.transform = "rotateY(180deg)";
+        target.addEventListener("click", backRotate);
+        listCard.classList.add("back");
+      }
+
+      function backRotate(event) {
+        const target = event.currentTarget;
+        target.style.transform = "rotateY(0deg)";
+        target.addEventListener("click", cardRotate);
+        target.removeEventListener("click", backRotate);
+        listCard.classList.remove("back");
+      }
+
+      listCard.addEventListener("click", cardRotate);
+    });
   })
   .catch((err) => console.error(err));
